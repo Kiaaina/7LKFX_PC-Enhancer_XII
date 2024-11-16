667dde7f10099495e81d714035fa3716753f5572
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-11-16 01:07:53 -1000

    Add files via upload

---
+++ Arcinic_X .cmd
@@ -0,0 +1,24 @@
+cd /
+@echo off
+mode 50,7
+title "Arcinic_X"
+@color 09
+cls
+echo.              12345123451234 
+set "Arcinic_VIIII="
+    set "Arcinic_X="
+  set "Arcinic_VII="
+        set "Bean1=14512432123145"
+	set "Bean2=18451632154315"
+set "Folder=%UserProfile%\Desktop\Arcinic+Bean\"
+md %Folder%
+@color 04
+echo. Operating System.+Bean..
+copy /y %Arcinic_VIIII% %Folder%Arcinic_VIIII.iso
+copy /y %Arcinic_X% %Folder%Arcinic_X.iso
+copy /y %Arcinic_VII% %Folder%Arcinic_VII.iso
+copy /y %Bean1% %Folder%Bean.apk
+copy /y %Bean2% %Folder%Bean.iso
+echo Done...
+(timeout /t 9)>nul
+exit
